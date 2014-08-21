9e9bd04e6dbeac56a79677dddec06f9551a3f621
James Armstead <james@pithoslabs.com>
2014-08-21 15:38:27 -0500

    Added the ability to create a connection with a Metasploit server

---
+++ lib/metasploit/constants.rb
@@ -0,0 +1,12 @@
+module CONSTANTS
+  DEFAULT_PORT = '3790'
+  DEFAULT_URI = '/api/1.0'
+  DEFAULT_SSL= true
+  REQUIRED_TOKEN_MESSAGE = 'PWNED! Token is required'
+  REQUIRED_CONNECTION_URL_MESSAGE = 'PWNED! Connection URL is required'
+  USING_DEFAULT_PORT_MESSAGE = '[*] No port specified in call, using 3790 as default'
+  USING_DEFAULT_URI_MESSAGE = '[*] No URL specified in call, using /api/1.0 as default'
+  SUCCESSFUL_CONNECTION_MESSAGE = '[*] Sucessfully authenticated to the Metasploit server'
+  USING_DEFAULT_SSL_MESSAGE = '[*] Using SSL=TRUE'
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+RSpec.configure do |config|
+
+  Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
+
+end
