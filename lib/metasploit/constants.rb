ce662ce5d207f86f40fce686d1ca06a23b4b8159
James Armstead <james@pithoslabs.com>
2014-08-22 08:55:12 -0500

    Adding the ability to import nexpose data

--- lib/metasploit/constants.rb
+++ lib/metasploit/constants.rb
@@ -2,12 +2,14 @@ module CONSTANTS
   DEFAULT_PORT = '3790'
   DEFAULT_URI = '/api/1.0'
   DEFAULT_SSL= true
+  RUNNING_IMPORT_STATUS = 'running'
   REQUIRED_TOKEN_MESSAGE = 'PWNED! Token is required'
   REQUIRED_CONNECTION_URL_MESSAGE = 'PWNED! Connection URL is required'
   USING_DEFAULT_PORT_MESSAGE = '[*] No port specified in call, using 3790 as default'
   USING_DEFAULT_URI_MESSAGE = '[*] No URL specified in call, using /api/1.0 as default'
   SUCCESSFUL_CONNECTION_MESSAGE = '[*] Sucessfully authenticated to the Metasploit server'
   USING_DEFAULT_SSL_MESSAGE = '[*] Using SSL=TRUE'
+  IMPORTING_DATA_MESSAGE = '[*] Importing...'
   REQUIRED_WORKSPACE_MESSAGE= 'PWNED! Workspace Name is required'
 
 end
