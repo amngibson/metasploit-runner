4dd3306757ece196b679b6e0f9c56bda7e0b10c6
James Armstead <james@pithoslabs.com>
2014-08-22 10:15:19 -0500

    Changing messages to be correct

--- lib/metasploit/constants.rb
+++ lib/metasploit/constants.rb
@@ -7,10 +7,11 @@ module CONSTANTS
   REQUIRED_CONNECTION_URL_MESSAGE = 'PWNED! Connection URL is required'
   REQUIRED_DEVICE_IP_TO_SCAN_MESSAGE = 'PWNED! Device IP to scan is required'
   USING_DEFAULT_PORT_MESSAGE = '[*] No port specified in call, using 3790 as default'
-  USING_DEFAULT_URI_MESSAGE = '[*] No URL specified in call, using /api/1.0 as default'
+  USING_DEFAULT_URI_MESSAGE = '[*] No URI specified in call, using /api/1.0 as default'
   SUCCESSFUL_CONNECTION_MESSAGE = '[*] Sucessfully authenticated to the Metasploit server'
   USING_DEFAULT_SSL_MESSAGE = '[*] Using SSL=TRUE'
   IMPORTING_DATA_MESSAGE = '[*] Importing...'
+  SCANNING_MESSAGE = '[*] Scanning all your things...'
   REQUIRED_WORKSPACE_MESSAGE= 'PWNED! Workspace Name is required'
 
 end
