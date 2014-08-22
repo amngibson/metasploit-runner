b7981ca5a5512d16a2e2f7b02e1dc86e85725919
James Armstead <james@pithoslabs.com>
2014-08-21 22:35:07 -0500

    Adding the call to start an import, refactoring to get rid of some dupicate code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 RSpec.configure do |config|
 
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
   Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
 
 end
